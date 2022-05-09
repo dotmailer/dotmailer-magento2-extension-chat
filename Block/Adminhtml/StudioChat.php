19d88fd52c935e59612c760b11547e046aba2f85
Fanis Strezos <Fanis.Strezos@dotmailer.com>
2022-05-09 11:55:18 +0000

    Merged PR 36906: Update references from TrialSetup to IntegrationSetup
    ## What's being changed
    
    Namespaces and classes updated on email module for go-bag-project updated to chat module as well.
    
    ## Why it's being changed
    
    Chat account creation was failing.
    
    ## How to review / test this change
    
    - In Email module switch to go-bag-develop branch
    - Create an account via microsite using chat route.
    - Make sure that account created successfully
    
    Related work items: #171600

--- Block/Adminhtml/StudioChat.php
+++ Block/Adminhtml/StudioChat.php
@@ -7,8 +7,8 @@ use Dotdigitalgroup\Email\Block\Adminhtml\EngagementCloudTrialInterface;
 use Dotdigitalgroup\Email\Block\Adminhtml\HandlesMicrositeRequests;
 use Dotdigitalgroup\Email\Helper\Data;
 use Dotdigitalgroup\Email\Helper\OauthValidator;
-use Dotdigitalgroup\Email\Model\Trial\TrialSetup;
-use Dotdigitalgroup\Email\Model\Trial\TrialSetupFactory;
+use Dotdigitalgroup\Email\Model\Integration\IntegrationSetup;
+use Dotdigitalgroup\Email\Model\Integration\IntegrationSetupFactory;
 use Magento\Backend\Block\Template\Context;
 
 /**
@@ -21,9 +21,9 @@ class StudioChat extends \Magento\Backend\Block\Template implements EngagementCl
     use HandlesMicrositeRequests;
 
     /**
-     * @var TrialSetupFactory
+     * @var IntegrationSetupFactory
      */
-    private $trialSetupFactory;
+    private $integrationSetupFactory;
 
     /**
      * @var Data
@@ -43,19 +43,19 @@ class StudioChat extends \Magento\Backend\Block\Template implements EngagementCl
     /**
      * StudioChat constructor.
      * @param Context $context
-     * @param TrialSetupFactory $trialSetupFactory
+     * @param IntegrationSetupFactory $integrationSetupFactory
      * @param Data $helper
      * @param Config $config
      * @param OauthValidator $oauth
      */
     public function __construct(
         Context $context,
-        TrialSetupFactory $trialSetupFactory,
+        IntegrationSetupFactory $integrationSetupFactory,
         Data $helper,
         Config $config,
         OauthValidator $oauth
     ) {
-        $this->trialSetupFactory = $trialSetupFactory;
+        $this->integrationSetupFactory = $integrationSetupFactory;
         $this->helper = $helper;
         $this->config = $config;
         $this->oauth = $oauth;
@@ -71,8 +71,8 @@ class StudioChat extends \Magento\Backend\Block\Template implements EngagementCl
     {
         //If API Creds Aren't set
         if (!$this->helper->isEnabled()) {
-            return $this->getTrialSetup()
-                ->getEcSignupUrl($this->getRequest(), TrialSetup::SOURCE_CHAT);
+            return $this->getIntegrationSetup()
+                ->getEcSignupUrl($this->getRequest(), IntegrationSetup::SOURCE_CHAT);
         }
 
         return $this->oauth->createAuthorisedEcUrl($this->config->getChatPortalUrl());
