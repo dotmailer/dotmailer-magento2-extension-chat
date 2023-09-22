c8e5b5a84666401f6e46725bbcbef74762e59b39
Alastair Mucklow <Alastair.Mucklow@dotmailer.com>
2023-09-22 16:41:07 +0000

    Merged PR 48049: Refactor getRequest() in EmailCapturePlugin
    ## What's being changed
    
    A plugin to a controller in the Email module.
    
    ## Why it's being changed
    
    - Using `$this->getRequest()` in controllers is not allowed - we have to use `$context->getRequest()`.
    
    ## How to review / test this change
    
    - Put a breakpoint in the plugin
    - Start a chat
    - Enter an email in the newsletter input
    - After submit the email should be picked up in the plugin via an AJAX request
    
    Related work items: #223644

--- Plugin/EmailcapturePlugin.php
+++ Plugin/EmailcapturePlugin.php
@@ -5,6 +5,9 @@ namespace Dotdigitalgroup\Chat\Plugin;
 use Dotdigitalgroup\Chat\Model\Config;
 use Dotdigitalgroup\Chat\Model\Profile\UpdateChatProfile;
 use Dotdigitalgroup\Email\Controller\Ajax\Emailcapture;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\Stdlib\Cookie\CookieReaderInterface;
 
 class EmailcapturePlugin
@@ -13,6 +16,11 @@ class EmailcapturePlugin
      * @var UpdateChatProfile
      */
     private $chatProfile;
+
+    /**
+     * @var RequestInterface
+     */
+    private $request;
 
     /**
      * @var CookieReaderInterface
@@ -24,25 +32,31 @@ class EmailcapturePlugin
      *
      * @param UpdateChatProfile $chatProfile
      * @param CookieReaderInterface $cookieReader
+     * @param Context $context
      */
     public function __construct(
         UpdateChatProfile $chatProfile,
-        CookieReaderInterface $cookieReader
+        CookieReaderInterface $cookieReader,
+        Context $context
     ) {
         $this->chatProfile = $chatProfile;
         $this->cookieReader = $cookieReader;
+        $this->request = $context->getRequest();
     }
 
     /**
      * After email capture execute
      *
      * @param Emailcapture $emailcapture
+     * @param ResultInterface $result
      */
-    public function afterExecute(Emailcapture $emailcapture)
+    public function afterExecute(Emailcapture $emailcapture, $result)
     {
         // if a chat profile ID is present, update chat profile data
         if ($chatProfileId = $this->cookieReader->getCookie(Config::COOKIE_CHAT_PROFILE, null)) {
-            $this->chatProfile->update($chatProfileId, $emailcapture->getRequest()->getParam('email'));
+            $this->chatProfile->update($chatProfileId, $this->request->getParam('email'));
         }
+
+        return $result;
     }
 }
